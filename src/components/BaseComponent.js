2aa1a946fdf0b9a1a9c4211ab1f4115ece742923
Paul Walker <pjwalker76@gmail.com>
2016-03-30 09:16:27 -0400

    Factor fetch bindings to base component

--- src/components/BaseComponent.js
+++ src/components/BaseComponent.js
@@ -5,12 +5,10 @@ export default class BaseComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-		console.log('Base Const', this.props.type);
   }
 
   componentDidMount(){
-		console.log('.1');
-		if (this.props.fetchData) {
+		if (this.props.fetchData && this[this.props.fetchData]) {
     	this.fetchData().then(this.onData.bind(this));
 		}
   }
@@ -20,18 +18,14 @@ export default class BaseComponent extends Component {
   }
 
   fetchData() {
-		console.log('.2');
-		console.log('fetchData', this.props.fetchData, this[this.props.fetchData]);
    	return Promise.resolve(this[this.props.fetchData]());
   }
 
   setData(data) {
-		console.log('.3');
     this.setState({data:data});
   }
 
   emitChange(payload) {
-		console.log('.4');
     EventDispatcher.dispatch(payload);
   }
 }
