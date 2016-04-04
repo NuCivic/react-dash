7c2720b59e750b4b772e6457a3f30808993f050c
Janette Day <janette@nucivic.com>
2016-04-04 13:31:43 -0500

    Updating default css, re-ordering includes on examples/index.js

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,8 @@
+import '../bower_components/bootstrap/dist/css/bootstrap.min.css';
+import 'nvd3/build/nv.d3.min.css';
+import 'react-select/dist/react-select.min.css';
+import 'fixed-data-table/dist/fixed-data-table.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { settings } from './settings';
@@ -8,11 +13,5 @@ import GAGoal from './components/GAGoal';
 import Choropleth from './components/GAChoropleth';
 import MyCustomLayout from './layouts/MyCustomLayout';
 import GADashboard from './app';
-
-
-import '../bower_components/bootstrap/dist/css/bootstrap.min.css';
-import 'nvd3/build/nv.d3.min.css';
-import 'react-select/dist/react-select.min.css';
-import 'fixed-data-table/dist/fixed-data-table.min.css';
 
 ReactDOM.render(<GADashboard {...settings} layout={MyCustomLayout}/>, document.getElementById('root'));
