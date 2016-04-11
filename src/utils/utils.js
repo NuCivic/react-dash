ea332fb89ea76f7d8baf9880f02963a747d91bbf
Paul Walker <pjwalker76@gmail.com>
2016-04-11 13:56:20 -0400

    Only use letters for unique key in level classnames to avoid confusion with the level value

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,7 +8,7 @@ export function makeKey(len)
 {
 	  let length = len || 5;
     var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
     for( var i=0; i < len; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
