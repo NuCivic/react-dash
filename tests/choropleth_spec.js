2e9940c08aa49d1aa4a0aa79f085923148938322
Paul Walker <pjwalker76@gmail.com>
2016-04-12 12:02:59 -0400

    Stub choropleth component test

--- tests/choropleth_spec.js
+++ tests/choropleth_spec.js
@@ -1,18 +1,16 @@
-var React = require('react');
-var TestUtils = require('react/lib/ReactTestUtils');
+const React = require('react');
+const TestUtils = require('react/lib/ReactTestUtils');
+let Choro = require('../src/components/Choropleth.jsx');
 
 describe('Libs load', () => {
-  console.log('m00', React, TestUtils);
   it('should be libs', () => {
-    expect(typeof React).not.toBe('undefined');
+    expect(typeof React).toBeTruthy();
     expect(typeof TestUtils).not.toBe('undefined');
   })
 });
 
-describe('2+2', () => {
-  let sum = 2+2;
-
-  it('should be 2', () => {
-    expect(sum).toBe(4);
+describe('Choropleth element should load', () => {
+  it('Should be truthy', () => {
+    expect(Choro).toBeTruthy();
   });
-});
+})
