eb7ac83ff8fe17b46e6384a16576f114629de594
Paul Walker <starsinmypockets@gmail.com>
2016-06-02 18:04:32 -0400

    clean up example app

--- examples/components/GAChoropleth.js
+++ examples/components/GAChoropleth.js
@@ -8,16 +8,6 @@ import topodata from 'json!../../examples/data/us.json';
 import domainData from 'dsv?delimiter=\t!../../examples/data/unemployment.tsv';
 
 export default class GAChoropleth extends Choropleth {
-/*  constructor(args) {
-    console.log('init',args);
-    super(args);
-    console.log('C',args,this);
-    return this;
-  }
-*/
-  getFilters() {
-    console.log('f', this);
-  }
 }
 
 Registry.set('GAChoropleth', GAChoropleth);
