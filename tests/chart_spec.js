c23b16d92f85ad65c61507ba4b7073aa9e0d3804
Paul Walker <starsinmypockets@gmail.com>
2016-06-09 12:55:21 -0400

    chart component instantiates with mocked global data from db.json

---
+++ tests/chart_spec.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import {Registry} from '../src/ReactDashboard';
+import {timeFormat as d3_timeFormat} from 'd3-time-format';
+import Chart from '../examples/components/GAChart';
+import DB from '../db.json';
+ 
+let settings = 
+      {
+				globalData: DB.data,
+        header:'Top',
+        type: 'GAChart',
+        iconClass: 'glyphicon glyphicon-tree-conifer',
+        settings: {
+          id:'lineChart2',
+          type: 'lineChart',
+          x: 'date',
+          y: 'price',
+          height: 340,
+          margin: {
+            left: 38
+          },
+          color: ['#EA7E7E'],
+          xAxis: {
+            tickFormat: d3_timeFormat('%Y')
+          }
+        },
+        cardStyle: 'card',
+        fetchData: {type:'function', name: 'getTopChartData'},
+
+      };
+
+describe('Instantiate Chart component', () => {
+  it('Component should render as React DOM node', () => {
+    let node = TestUtils.renderIntoDocument(React.createElement(Registry.get('GAChart'), settings));
+    console.log(node.state);
+    expect(node.props.settings.id).toBe('lineChart2');
+  });
+});
+
+// @@TODO
+// * add test for CSV backend
