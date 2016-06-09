f40a0770c7e1371d483d39623ef9c08022d0857e
Paul Walker <starsinmypockets@gmail.com>
2016-06-09 13:02:03 -0400

    Stub metric component test

---
+++ tests/metric_spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import {Registry} from '../src/ReactDashboard';
+import Metric from '../examples/components/GAMetric';
+ 
+let settings = {
+                type:'GAMetric',
+                key: '2a',
+                cardStyle: 'metric',
+                background: '#9F3E69',
+                metric: 'getRandomMetric',
+                caption: 'Get Multi',
+                iconClass: 'glyphicon glyphicon-user'
+							};
+
+describe('Instantiate Metric component', () => {
+  it('Component should render as React DOM node', () => {
+    let node = TestUtils.renderIntoDocument(React.createElement(Registry.get('GAMetric'), settings));
+    expect(node.props.type).toBe('GAMetric');
+  });
+});
+
+// @@TODO
+// * add test for CSV backend
