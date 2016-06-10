3c9ca2b7b4f9bbdf7d7965f4a1658182d9fee9ce
Paul Walker <starsinmypockets@gmail.com>
2016-06-10 11:26:44 -0400

    Add jquery dev dep and look for text in dom to confirm text component works

--- tests/text_spec.js
+++ tests/text_spec.js
@@ -2,18 +2,31 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import {Registry} from '../src/ReactDashboard';
 import Text from '../src/components/Text';
- 
+import $ from 'jquery';
+
 let settings = {
 								header: 'This is an awesome text',
 								type: 'Text',
-								content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut erat dui, sodales eleifend placerat a, dictum sed tortor.</p><p> Quisque porttitor urna in est vehicula, a molestie nunc pharetra. Cras vehicula nisi dui, ut aliquam nunc vulputate lacinia. Curabitur vitae interdum dolor, sed venenatis tellus. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat metus et ipsum lobortis, at porttitor nunc laoreet.</p><p>Nullam et ligula at enim pretium accumsan. In et facilisis enim, vel consectetur justo. Duis eleifend sit amet neque eu interdum. Sed ornare orci diam, ac finibus ipsum posuere vel. Duis maximus velit ipsum, et mattis massa tempus sit amet. Suspendisse potenti.</p>',
-								cardStyle: 'card',							
+								content: '<p class="foo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut erat dui, sodales eleifend placerat a, dictum sed tortor.</p><p> Quisque porttitor urna in est vehicula, a molestie nunc pharetra. Cras vehicula nisi dui, ut aliquam nunc vulputate lacinia. Curabitur vitae interdum dolor, sed venenatis tellus. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat metus et ipsum lobortis, at porttitor nunc laoreet.</p><p>Nullam et ligula at enim pretium accumsan. In et facilisis enim, vel consectetur justo. Duis eleifend sit amet neque eu interdum. Sed ornare orci diam, ac finibus ipsum posuere vel. Duis maximus velit ipsum, et mattis massa tempus sit amet. Suspendisse potenti.</p>',
+								cardStyle: 'card',
+                className: 'foo'
               };
 
 describe('Instantiate Text component', () => {
-  it('Component should render as React DOM node', () => {
-    let node = TestUtils.renderIntoDocument(React.createElement(Registry.get('Text'), settings));
+  let node;
+  beforeEach(() => {
+    node = TestUtils.renderIntoDocument(React.createElement(Registry.get('Text'), settings));
+  });
+
+  it('Component should render as React class with props', () => {
     expect(node.props.type).toBe('Text');
+  });
+
+  it('React DOM node contains text element with expected text', () => {
+    let domNode = TestUtils.scryRenderedDOMComponentsWithClass(node, 'dashboard-text');
+    expect(domNode.length).toBe(1);
+    let text = $($(domNode[0]).find('p')[0]).text(); // sorry
+    expect(text).toContain('Lorem ipsum');
   });
 });
 
