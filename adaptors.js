80c66c75270ce79a515d0e8bd055002d074cb879
Paul Walker <starsinmypockets@gmail.com>
2016-06-23 10:17:56 -0400

    Stub adaptors class

---
+++ adaptors.js
@@ -0,0 +1,54 @@
+/**
+ * ADAPTOR CLASS
+ * 
+ * Provides a function which will format data from the standard format
+ * returned by our backends. It applies transformations to data. The transformed data
+ * can then be passed to a third party library (for example NVD3, PLOTLY, etc) for 
+ * rendering.
+ *
+ * By maintaining the adaptors class, we can keep our glue code isolated, and avoid repetition
+ * of tedious transformations
+ */
+export default class Adaptor {
+  /**
+   * @param opts.lib - the library (eg - nvd3)
+   * @param opts.func - the function name (eg pieChart)
+   */
+  lookup (opts) {
+    const func = _funcs[_lookup[opts.lib[opts.type]]];
+    if (func && typeof func === 'function') {
+      return func;
+    } else {
+      console.warn('WARNING: Adaptor library not found', opts);
+      return false;
+    }
+  }
+}
+
+/**
+ * PRIVATES 
+ * */
+/* Figure out which function we need to return */
+const _lookup = {
+  nvd3: {
+    lineChart: 'base',
+    linePlusBarChart: 'base',
+    discreteBarChart: 'base',
+    pieChart: 'pieChart',
+  }    
+}
+
+/* A hash of functions */
+const _funcs = {
+  nvd3: {
+    base: function (data, opts) {
+      return {
+        values: data
+      }  
+    },
+
+    pieChart: function () {
+      
+    }
+  }
+}
