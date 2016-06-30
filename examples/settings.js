ac07a4669b24fe85091a62a23eaa47936055e748
Paul Walker <starsinmypockets@gmail.com>
2016-06-30 15:58:26 -0400

    Clean up settings

--- examples/settings.js
+++ examples/settings.js
@@ -384,23 +384,12 @@ export var settings = {
 										}
 									}
 								},
-						 ]
-           },
-           {
-             type: 'Div',
-             className: 'col-md-6',
-             children: []
-           },
-         ]
-       },
-       {
-         type: 'Div',
-         className: 'fifth-row row',
-         children: [
-           {
-             type: 'Div',
-             className: 'col-md-12',
-             children: [
+              ]
+            },
+            {
+              type: 'Div',
+              className: 'col-md-6',
+              children: [
 								{
 									header:'Right',
 									type: 'GAChart',
@@ -425,12 +414,11 @@ export var settings = {
 									content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut erat dui, sodales eleifend placerat a, dictum sed tortor.</p><p> Quisque porttitor urna in est vehicula, a molestie nunc pharetra. Cras vehicula nisi dui, ut aliquam nunc vulputate lacinia. Curabitur vitae interdum dolor, sed venenatis tellus. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat metus et ipsum lobortis, at porttitor nunc laoreet.</p><p>Nullam et ligula at enim pretium accumsan. In et facilisis enim, vel consectetur justo. Duis eleifend sit amet neque eu interdum. Sed ornare orci diam, ac finibus ipsum posuere vel. Duis maximus velit ipsum, et mattis massa tempus sit amet. Suspendisse potenti.</p>',
 									cardStyle: 'card',
 								}
-						 ]
-           },
+              ]
+            }
          ]
-       },
-
-      ]
-    },
+      },
+    ]
+  }
   ]
 };
