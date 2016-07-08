95d065d8740ae7066779da43a2981637116c6f5c
Paul Walker <starsinmypockets@gmail.com>
2016-07-08 09:55:38 -0400

    Filters work if fetched data is present

--- examples/customDataHandlers.js
+++ examples/customDataHandlers.js
@@ -1,30 +1,34 @@
 import DataHandler from '../src/utils/DataHandler';
 
-let passThroughHandler = function (a,b,c,d,e,f) {
-  console.log('pass',a,b,c,d,e,f);
-  return [1,2,3,4,5];
+let passThroughHandler = function (componentData, dashboardData, handler, e, pipelineData) {
+  let _data = pipelineData || componentData;
+  console.log('passThroughHandler', _data, componentData, dashboardData, handler, e , pipelineData);
+  return _data;;
 }
 
 DataHandler.set('passThroughHandler', passThroughHandler);
 
 let customDataHandler = function (componentData, dashboardData, handler, e, pipelineData) {
-  console.log('c', this, componentData, dashboardData, handler, e, pipelineData);
+  let _data = pipelineData || componentData;
+  console.log('c', this, _data, componentData, dashboardData, handler, e, pipelineData);
   let dates = e.value.split('_') || 'ALL';
-  this.fetchData(d => {
-    console.log('FETCH', d);
-  });
-  
-  if (dates === 'ALL') return 
-  console.log('dates', dates);
-  function randomDate(start, end) {
-		return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-	}
-
-  let d1 = randomDate(new Date(2012, 0, 1), new Date());
-  let d2 = randomDate(new Date(2012, 0, 1), new Date());
-  let d3 = randomDate(new Date(2012, 0, 1), new Date());
-  let r = [[{date:d1,price:2}, {date:d2,price:3}, {date:d3,price:4}]];
-  return r;
+  if (dates[0] === 'all') {
+    // just reload the component
+    console.log('123');
+    return this.fetchData();
+  } else {
+    let low = Date.parse(dates[0]);
+    let high = Date.parse(dates[1]);
+    let vals = _data[0].values;
+    console.log(low,high,vals)
+    let filteredVals = vals.filter(d => { return (d.x >= low && d.x <= high) }).map(d => {
+        let date = new Date(d.x);
+        let r = {date: date, price: d.y};
+        return r;
+    });
+    console.log(filteredVals);
+    return filteredVals;   
+  }
 }
 
 DataHandler.set('customDataHandler', customDataHandler);
