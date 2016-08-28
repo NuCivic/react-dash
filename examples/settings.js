1fd77056e9753c2f2cfc17936c312bb3ba2560be
Paul Walker <starsinmypockets@gmail.com>
2016-08-28 13:22:10 -0400

    Remove extraneous handler from ex settings

--- examples/settings.js
+++ examples/settings.js
@@ -82,7 +82,7 @@ export var settings = {
         type: 'multiBarHorizontalChart',
         x: 'age',
         y: 'count_age',
-        color: ['blue'],// '#ffe6e6', '#ffcccc', '#ffb3b3', '#ff9999', '#ff8080', '#ff6666', '#ff4d4d', '#ff3333', '#ff1a1a']
+        color: ['blue'],
       },
       dataHandlers: [
         {
@@ -104,7 +104,85 @@ export var settings = {
           ]
         }        
       ]
-    }
+    },
+    {
+      header:'Top',
+      type: 'Chart',
+      iconClass: 'glyphicon glyphicon-tree-conifer',
+      settings: {
+          id:'lineChart2',
+          type: 'lineChart',
+          x: 'date',
+          height: 340,
+          margin: {
+              left: 38
+          },
+          color: ['#EA7E7E'],
+          //xAxis: {
+          //    tickFormat: dateFormatter('%Y')
+          //}
+      },
+      dataHandlers: [
+          {
+              name: 'common.parseDateField',
+              field: 'date'
+          },
+          {
+              name: 'common.fieldsToXYSeries',
+              field: 'price',
+              xField: 'date'
+          },
+          {
+              name: 'NVD3.returnChartSeries',
+              series: [
+                  {name: 'Price', color:'#FF0000'},
+              ]
+          }
+      ],
+      cardStyle: 'card',
+      fetchData: {
+          type:'backend',
+          backend: 'csv',
+          url: 'http://demo.getdkan.com/sites/default/files/data_0.csv'
+      },
+      filters: [
+        {
+          type: 'ReactSelect',
+          options: [
+            {
+              label: 'ALL',
+              value: 'all'
+            },
+            {
+              label: '1949 - 1976',
+              value: '1949_1976'
+            },
+            {
+              label: '1976 - 2012',
+              value: '1976_2012'
+            }
+          ],
+          dataHandlers: [
+            'customDataHandler',
+            {
+              name: 'common.parseDateField',
+              field: 'date'
+            },
+            {
+              name: 'common.fieldsToXYSeries',
+              field: 'price',
+              xField: 'date'
+            },
+            {
+              name: 'NVD3.returnChartSeries',
+              series: [
+                {name: 'Price', color:'#FF0000'},
+              ]
+            }
+          ]
+        },
+      ],                    
+    },
   ]
   
   
