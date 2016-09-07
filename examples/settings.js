678e3f87d3e43761bde4c291893d3bffda355163
Paul Walker <starsinmypockets@gmail.com>
2016-09-07 17:38:33 -0400

    paramRouting - add cids to settings file

--- examples/settings.js
+++ examples/settings.js
@@ -13,14 +13,17 @@ export var settings = {
   components: [
     {
       type: 'Region',
+      cid: 'r1',
       className: 'row',
       children: [
        {
         type: 'Region',
+        cid: 'r2',
         className: 'zeroth-row col-md-6',
         children: [
            {
              type: 'img',
+             cid: 'img1',
              src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTd9zBTlpPrH5EaWGrQISVCjA1E3AAzXCI_go0ml5QHI-58Cld73VBBRv8",
              style: {borderStyle:'double',borderWidth: '10px', borderColor:'teal', padding: '10px', margin: '10px'},
            },
@@ -28,10 +31,12 @@ export var settings = {
        },
        {
         type: 'Region',
+        cid: 'r3',
         className: 'zeroth-row col-md-6',
         children: [
            {
              type: 'h4',
+             cid: 'html1',
              dangerouslySetInnerHTML: {__html: 'A text description set from the settings file, using native react html element.'},
              style: {margin: '20px'}
            },
@@ -42,17 +47,20 @@ export var settings = {
     // the action listener will re-render the multi-component with the selected value
     // as the multi-components initVal
     // Target the multicomponent by its id field
+    // in app.js we use id to determine the update type and the target (app.js)
     {
       type: 'Autocomplete',
+      cid: 'ac1',
       placeholder: 'Select to repopulate Multi-component, below',
       style: {marginBottom: '1em'},
-      id: 'multicomponent-autocomplete',
-      name: 'multicomponent-autocomplete',
+      id: 'multicomponent-multi',
+      name: 'multicomponent-multi',
       options: [{value: 'a', label: 'a'}, {value: 'b', label: 'b'}]
     },
     {
       header: 'A Multi Component',
       type: 'Multi',
+      cid: 'm1',
       id: 'multicomponent',
       cardStyle: 'content',
       style: {padding: '1em', background: '#DAEEF5'},
@@ -61,12 +69,14 @@ export var settings = {
           a: [ // each set of elements is an array - even if it contains a single child
             {
               type: 'h3',
+              cid: 'html2',
               dangerouslySetInnerHTML: { __html: "MULTI COMPONENT CONTENT - A" }
             }	
           ],
           b: [ 
           {
             type: 'h3',
+            cid: 'html3',
             dangerouslySetInnerHTML: { __html: 'MULLTI COMPONENT CONTENT - B'}
           }
           ]
@@ -76,11 +86,13 @@ export var settings = {
 
     {
       type: 'Region',
+      cid: 'r4',
       className: 'dashboard-top-filter',
       header: 'Foobar',
       children: [
         {
           type: 'Autocomplete',
+          cid: 'ac2',
           name: 'county-autocomplete',
           multi: true,
           url: baseUrl+'/dashboard_autocomplete/GBPW_Counties',
@@ -92,9 +104,11 @@ export var settings = {
     },
     {
       type: 'Region',
+      cid: 'r5',
       children: [
         {
           type: 'Metric',
+          cid: 'met1',
           cardStyle: 'metric',
           value: 'Value from Config',
           background: 'muave',
@@ -102,6 +116,7 @@ export var settings = {
         },
         {
           type: 'Metric',
+          cid: 'met2',
           cardStyle: 'metric',
           background: 'navy',
           iconClass: 'fa-clock-o',
@@ -114,6 +129,7 @@ export var settings = {
     {
       header:'Gold Prices',
       type: 'Chart',
+      cid: 'ch1',
       iconClass: 'glyphicon glyphicon-tree-conifer',
       settings: {
           id:'lineChart2',
@@ -228,14 +244,17 @@ export var settings = {
     }, */
 		{
      type: 'Region',
+     cid: 'r6',
      header: 'Unemployment by U.S. County',
      children: [
         {
           type: 'h4',
+          cid: 'html45',
           dangerouslySetInnerHTML: {__html: 'Choropleth Heat Map -- topojson'}
         },
         {
           type: 'Choropleth',
+          cid: 'choro1',
           format: 'topojson',
           fetchData: {
             url: '/data/unemployment.tsv',
@@ -267,9 +286,11 @@ export var settings = {
         },
         {
           type: 'Region',
+          cid: 'r650',
           children: [
             {
               type: 'Goal',
+              cid: 'g1',
               title: '',
               caption: 'number of schools enrollments',
               link: 'http://tootherplace.com',
@@ -297,6 +318,7 @@ export var settings = {
             },
             {
               type: 'Goal',
+              cid: 'g2',
               title: '',
               caption: 'number of schools enrollments',
               link: 'http://tootherplace.com',
@@ -328,14 +350,17 @@ export var settings = {
     },
 		{
       type: 'Region',
+      cid: 'r131',
       children: [
 				{
           header: 'This is an awesome text',
 					type: 'Text',
+          cid: 'html111',
 					content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut erat dui, sodales eleifend placerat a, dictum sed tortor.</p><p> Quisque porttitor urna in est vehicula, a molestie nunc pharetra. Cras vehicula nisi dui, ut aliquam nunc vulputate lacinia. Curabitur vitae interdum dolor, sed venenatis tellus. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat metus et ipsum lobortis, at porttitor nunc laoreet.</p><p>Nullam et ligula at enim pretium accumsan. In et facilisis enim, vel consectetur justo. Duis eleifend sit amet neque eu interdum. Sed ornare orci diam, ac finibus ipsum posuere vel. Duis maximus velit ipsum, et mattis massa tempus sit amet. Suspendisse potenti.</p>',
 				},
 				{
 					type: 'DataTable',
+          cid: 'dt1',
 					header: 'Mi titulo',
           data: [
     				['a1', 'b1', 'c1'],
@@ -376,9 +401,11 @@ export var settings = {
 		},
     {
       type: 'Region',
+      cid: 'r344',
       children: [
         {
           type: 'Chart',
+          cid: 'chart12',
           header: 'Physician Distribution by Race',
           cardStyle: 'card',
           settings: {
