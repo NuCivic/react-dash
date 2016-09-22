e62b4e4296050322de93536c6df71688ef1f86b5
Paul Walker <starsinmypockets@gmail.com>
2016-09-22 13:31:17 -0400

    Add scaffolding for github-pages

---
+++ scripts/build-demo.sh
@@ -0,0 +1,3 @@
+webpack --config webpack.config.demo.js
+git commit -am "Build github pages @`date +%Y-%m-%d`"
+git push origin gh-pages
---
+++ scripts/start.sh
@@ -0,0 +1,2 @@
+./node_modules/.bin/json-server --watch db.json --port=3004 &
+node server.js --port=3000 --hot=true
