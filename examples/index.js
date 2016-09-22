d5179a76c5aeaaef4795fee18290232ef4acb8f1
Paul Walker <starsinmypockets@gmail.com>
2016-09-22 12:40:52 -0400

    gh pages Webpack 9

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,15 @@
 console.log(1, new Date());
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'nvd3/build/nv.d3.min.css';
-//import 'react-select/dist/react-select.min.css';
-//import 'fixed-data-table/dist/fixed-data-table.min.css';
+import 'nvd3/build/nv.d3.min.css';
+import 'react-select/dist/react-select.min.css';
+import 'fixed-data-table/dist/fixed-data-table.min.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
-//import customDataHandlers from './customDataHandlers';
-//import GADashboard from './GADashboard';
-//import { settings } from './settings';
-//import App from './App';
-//import { Router, Route, browserHistory } from 'react-router';
+import customDataHandlers from './customDataHandlers';
+import GADashboard from './GADashboard';
+import { settings } from './settings';
+import App from './App';
+import { Router, Route, browserHistory } from 'react-router';
 
 console.log(2,'..', React);
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
