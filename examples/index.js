55261bf706e76ac28d97138b1cd7b6a4b8c5cc06
Paul Walker <starsinmypockets@gmail.com>
2016-09-22 12:11:42 -0400

    gh pages Webpack 7

--- examples/index.js
+++ examples/index.js
@@ -1,14 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'nvd3/build/nv.d3.min.css';
-import 'react-select/dist/react-select.min.css';
-import 'fixed-data-table/dist/fixed-data-table.min.css';
-import ReactDOM from 'react-dom';
+console.log(1);
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'nvd3/build/nv.d3.min.css';
+//import 'react-select/dist/react-select.min.css';
+//import 'fixed-data-table/dist/fixed-data-table.min.css';
+//import ReactDOM from 'react-dom';
 import React from 'react';
-import customDataHandlers from './customDataHandlers';
-import GADashboard from './GADashboard';
-import { settings } from './settings';
-import App from './App';
-import { Router, Route, browserHistory } from 'react-router';
+//import customDataHandlers from './customDataHandlers';
+//import GADashboard from './GADashboard';
+//import { settings } from './settings';
+//import App from './App';
+//import { Router, Route, browserHistory } from 'react-router';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(2);
+//ReactDOM.render(<App />, document.getElementById('root'));
