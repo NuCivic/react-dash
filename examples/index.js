70550db64397f1e0ea758ab9635a40625ef47614
Paul Walker <starsinmypockets@gmail.com>
2016-09-29 17:34:16 -0400

    Test for jekyll env - 1

--- examples/index.js
+++ examples/index.js
@@ -9,4 +9,6 @@ import { settings } from './settings';
 import App from './App';
 import { Router, Route, browserHistory } from 'react-router';
 
+console.log('JJ', jeckyll);
+
 ReactDOM.render(<App />, document.getElementById('root'));
