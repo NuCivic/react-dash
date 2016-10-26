b2850b5c81885a9edd72c1b8c3f5d265b796a2b8
pj <starsinmypockets@gmail.com>
2016-10-26 16:42:07 -0400

    Remove example snippet form settings.js

--- examples/settings.js
+++ examples/settings.js
@@ -59,25 +59,6 @@ const stateIds =
               { value: 50, label: 'USA?'}
 						]
 
-export var settings = {
-  title: 'Hello World',
-  components: [
-    {
-      type: 'Chart',
-      cardStyle: 'Chart',
-      header: 'My Pie Chart',
-      data: [[{x: 1, y: 40}, {x: 2, y: 40}, {x: 3, y: 20}]],
-      dataHandlers: ['NVD3.toPieChartSeries'],
-      settings: {
-        type: 'pieChart',
-        x: 'x',
-        y: 'y',
-        height: '600'
-      },
-    }
-  ]
-}
-
 var _settings = {
 //export var settings = {
   title: 'React-Dash Demo -- Climate Indices by Year',
