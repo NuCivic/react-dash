b19d95841b39f7a88cf8027c5508dddac2c3e99b
pj <starsinmypockets@gmail.com>
2016-12-23 12:03:45 -0500

    Add key to autocomplete el

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Registry from '../utils/Registry';
 import BaseFilter from './BaseFilter';
 import ReactSelect from './ReactSelect';
+import { makeKey } from '../utils/utils';
 
 export default class Autocomplete extends BaseFilter {
   constructor(props) {
     super(props);
-    this.state.actionType = "AUTOCOMPLETE_CHANGE"
+    this.state.actionType = "AUTOCOMPLETE_CHANGE";
+    this.state.elKey = makeKey();
   }  
   
   render(){
@@ -14,7 +16,7 @@ export default class Autocomplete extends BaseFilter {
     if (val && !this.props.multi) val = val[0];
     console.log('AC', val, this);
     return (
-      <ReactSelect.Async value={val} loadOptions={this.loadOptions.bind(this)} {...this.props} onChange={this.onChange.bind(this)}/>
+      <ReactSelect.Async value={val} key={this.state.elKey} loadOptions={this.loadOptions.bind(this)} {...this.props} onChange={this.onChange.bind(this)}/>
     );
   }
 }
