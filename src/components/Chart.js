06bb492d5ae55e02864c0e71728941d72945e51f
pj <starsinmypockets@gmail.com>
2017-01-13 12:42:42 -0500

    Generate tickFormat formatter dynamically from settings.js so that we don't need to keep functions in the settings file (which will block us from storing settings in db)

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,7 +4,8 @@ import NVD3Chart from 'react-nvd3';
 import BaseComponent from './BaseComponent';
 import Loader from './Loader';
 import Filter from './Filter';
-import { makeKey } from '../utils/utils'
+import { makeKey } from '../utils/utils';
+import { isFunction } from 'lodash';
 import { format } from 'd3';
 
 export default class Chart extends BaseComponent {
@@ -15,10 +16,33 @@ export default class Chart extends BaseComponent {
       this.state.key = makeKey();
     }
   }
-
   
+  // given a d3 format specifier, return a d3 formatting
+  // function for use by react-nvd3 component
+  getFormatter(specifier, time=false) {
+    console.log('SPEC', specifier);
+    if (typeof specifier === 'string') {
+      return format(specifier);
+    }
+    if (isFunction(specifier)) {
+      return specifier;
+    }
+  }
+  
+  getFormattedSettings() {
+    let _settings = Object.assign({}, this.props.settings);
+    
+    Object.keys(_settings).forEach(k => {
+      if (_settings[k].tickFormat) {
+        _settings[k].tickFormat = this.getFormatter(_settings[k].tickFormat);
+      }
+    });
+
+    return _settings;
+  }
+
   render() {
-    let settings = Object.assign({datum: this.props.data, key: this.state.key}, this.props.settings);
+    let settings = Object.assign({datum: this.props.data, key: this.state.key}, this.getFormattedSettings());
     return (
         <NVD3Chart {...settings} key={this.state.key}/>
      )
