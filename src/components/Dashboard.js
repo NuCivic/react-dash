ca6c2b2de1ab6d4545cb3ea598ad09c7eb15a6f7
pj <starsinmypockets@gmail.com>
2017-01-24 17:55:08 -0500

    Add check for multi region in getUrlFilter - not currently supported

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -93,11 +93,14 @@ export default class Dashboard extends BaseComponent {
 
   getFilterByField(field) {
     let filter;
-
+    
     this.props.regions.forEach(region => {
-      return region.children.forEach(child => {
-        if (child.field === field) filter = child; 
-      })
+      if (region.children) {
+        return region.children.forEach(child => {
+          if (child.field === field) filter = child; 
+        })
+      }
+      // @@TODO this should support multi regions which have elementsobject keyed to array of components
     });
 
     return filter;
