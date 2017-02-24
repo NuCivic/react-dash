f7b4ffe0a2036d4468c3e2ab4b70882eede2dfa8
pj <starsinmypockets@gmail.com>
2017-02-24 10:33:35 -0500

    Build github pages @2017-02-24

--- src/ReactDashboard.js
+++ src/ReactDashboard.js
@@ -1,5 +1,5 @@
 import './stylesheets/main.scss';
-console.log('React Dashboard -- 0.5.x');
+console.log('React Dashboard -- 0.5.0');
 
 // COMPONENTS
 export {default as Autocomplete } from './components/Autocomplete';
