6f5ec4923e582a89f65db3f929e31f4c1090a2dc
pj <starsinmypockets@gmail.com>
2017-05-11 16:01:44 -0400

    tag

--- src/ReactDashboard.js
+++ src/ReactDashboard.js
@@ -1,5 +1,5 @@
 import './stylesheets/main.scss';
-console.log('React Dashboard -- 0.6.12.3');
+console.log('React Dashboard -- 0.6.12.4');
 
 // COMPONENTS
 export {default as Autocomplete } from './components/Autocomplete';
