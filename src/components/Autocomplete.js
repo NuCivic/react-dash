0db18823efc6de29a3ec22a403bc56ca99b9abc1
pj <starsinmypockets@gmail.com>
2017-05-22 16:47:44 -0400

    Link label to input in autocomplete for screen-reader accessibility

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -15,11 +15,19 @@ export default class Autocomplete extends BaseFilter {
   
   render(){
     let val = this.getFilterValue();
+    let label = this.props.label || 'Filter Label';
+    let labelClass = (this.props.label) ? '' : 'sr-only';
+    let inputProps = {};
     
+    inputProps.id = this.state.key;
+
     if (val && !this.props.multi) val = val[0];
     
     return (
-      <ReactSelect.Async value={val} loadOptions={this.loadOptions.bind(this)} disabled={this.isDisabled()} {...this.props} onChange={this.onChange.bind(this)} key={this.state.key} />
+      <div className='autocomplete-filter-container'>
+        <label htmlFor={this.state.key} className={labelClass}>Filter Label</label>
+        <ReactSelect.Async value={val} loadOptions={this.loadOptions.bind(this)} disabled={this.isDisabled()} {...this.props} onChange={this.onChange.bind(this)} key={this.state.key} inputProps={inputProps} />
+      </div>
     );
   }
 }
