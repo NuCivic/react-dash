1e2a360b7b5852b930142b2d75b80603b15c212f
paul walker <paulwalker@pauls-MacBook-Pro.local>
2017-08-12 19:11:09 -0700

    Build github pages @2017-08-12

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './nv.d3.min.css';
 import 'react-select/dist/react-select.min.css';
-import 'fixed-data-table/dist/fixed-data-table.min.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import customDataHandlers from './customDataHandlers';
